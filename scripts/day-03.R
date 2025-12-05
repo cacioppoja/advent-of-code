d5d436f7603a7567b266ab00905f2b67b3ba0508
Jessica Cacioppo <cacioppoja@gmail.com>
2025-12-05 06:59:51 -0600

    day 03, part 2

--- scripts/day-03.R
+++ scripts/day-03.R
@@ -27,6 +27,7 @@ get_joltage <- function(bank, num_batteries, index_battery, max_index) {
   return(list(max_value, max_index))
 }
 
+# For 2 batteries in bank
 bank_battery_on <- rep(0, length(example_input))
 for (j in 1:length(example_input)) {
   battery1 <- get_joltage(example_input[j], 2, 1, 0)
@@ -34,15 +35,63 @@ for (j in 1:length(example_input)) {
   bank_battery_on[j] <- as.integer(paste0(battery1[[1]], battery2[[1]]))
 }
 
+# Answer from running on example input
 example_answer <- sum(bank_battery_on)
 
-# Answer
+# Run on real input
 input <- readLines("data/day-03_part1.txt")
 bank_battery_on <- rep(0, length(input))
 for (j in 1:length(input)) {
-  battery1 <- get_joltage(input[j], 2, 1, 0)
-  battery2 <- get_joltage(input[j], 2, 2, battery1[[2]])
+  battery1 <- get_joltage(input[j], 12, 1, 0)
+  battery2 <- get_joltage(input[j], 12, 2, battery1[[2]])
   bank_battery_on[j] <- as.integer(paste0(battery1[[1]], battery2[[1]]))
 }
 
 answer <- sum(bank_battery_on)
+
+## Part 2 ----------------------------------------------------------------------
+# Adjust to run for 12 batteries in bank
+
+# From beginning this seemed like I should use recursion but I didn't set it up
+#  Try doing that later
+
+### Example --------------------------------------------------------------------
+bank_battery_on <- rep(0, length(example_input))
+battery <- vector("list", length = 12)
+for (j in 1:length(example_input)) {
+  for (k in 1:12){
+    if (k == 1) {
+      battery[[k]] <- get_joltage(example_input[j], 12, k, 0)
+      bank_battery_on[j] <- 
+        # as.integer(
+        paste0(battery[[k]][[1]])
+      # )
+    }
+    else if (k > 1) {
+      battery[[k]] <- get_joltage(example_input[j], 12, k, battery[[k-1]][[2]])
+      bank_battery_on[j] <- 
+        # as.integer(
+        paste0(bank_battery_on[j], battery[[k]][[1]])
+      # )
+    }
+  }
+}
+example_answer_part2 <- sum(as.numeric(bank_battery_on))
+
+### Real instructions ----------------------------------------------------------
+bank_battery_on <- rep(0, length(input))
+battery <- vector("list", length = 2)
+for (j in 1:length(input)) {
+  for (k in 1:12){
+    if (k == 1) {
+      battery[[k]] <- get_joltage(input[j], 12, k, 0)
+      bank_battery_on[j] <- paste0(battery[[k]][[1]])
+    }
+    else if (k > 1) {
+      battery[[k]] <- get_joltage(input[j], 12, k, battery[[k-1]][[2]])
+      bank_battery_on[j] <- paste0(bank_battery_on[j], battery[[k]][[1]])
+    }
+  }
+}
+answer_part2 <- sum(as.numeric(bank_battery_on))
+format(answer_part2, scientific = FALSE)

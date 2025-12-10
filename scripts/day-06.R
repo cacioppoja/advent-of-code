0d4444a00c1ddc697e73eaf924109b493276a621
Jessica Cacioppo <cacioppoja@gmail.com>
2025-12-09 21:13:39 -0600

    Day 06, Part 2

--- scripts/day-06.R
+++ scripts/day-06.R
@@ -8,12 +8,6 @@ example_input <- readLines("data/day-06_part1_example.txt")
 # Read real input
 real_input <- readLines("data/day-06_part1.txt")
 
-length(example_input)
-nchar(example_input)
-length(real_input)
-nchar(real_input)
-
-
 input_to_dataframe <- function(input) {
   # Collapse out white space
   input_list <- strsplit(input, " ")
@@ -54,3 +48,40 @@ answer <- sum(
     apply_operator(input_clean[[1]], input_clean[[2]], input_clean[[3]])
   )
 )
+
+# Part 2 -----------------------------------------------------------------------
+cephalopod_math <- function(input) {
+  # Each character in row,col
+  matrix <- matrix(
+    unlist(strsplit(input, "")), 
+    nrow = length(input),
+    byrow = TRUE
+  )
+  
+  # Index of first operator character
+  first_index <- which(matrix[dim(matrix)[1],] != " ")
+  # Last is 2 before the first because of column space separator
+  last_index <- c(first_index[2:length(first_index)] - 2, dim(matrix)[2] - 1)
+  last_index[length(last_index)] <- last_index[length(last_index)] + 1
+  
+  # Repeat the operator character and separate columns by |
+  clean_matrix <- matrix
+  for (i in 1:length(first_index)) {
+    char <- matrix[dim(matrix)[1], first_index[i]]
+    clean_matrix[dim(matrix)[1], first_index[i]:(last_index[i] - 1)] <- char
+    clean_matrix[dim(matrix)[1], last_index[i]] <- "|"
+  }
+  # Write the equations
+  equation <- unlist(
+    strsplit(paste(clean_matrix, collapse = ""), split = "|", fixed = TRUE)
+  )
+  total <- 0
+  for (i in 1:length(equation)) {
+    total <- total + eval(parse(text = equation[i]))
+  }
+  return(list(matrix, clean_matrix, equation, total))
+}
+
+example_part2 <- cephalopod_math(example_input)
+real_part2 <- cephalopod_math(real_input)
+format(real_part2[[4]], scientific = FALSE)
